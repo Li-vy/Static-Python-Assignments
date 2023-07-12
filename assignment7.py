bafae7473e097ef44d468bb130dc59d2d85eb564
Li-vy <narkarvishhal2005@gmail.com>
2023-07-12 21:25:03 +0530

    Assignment 7 update upload

--- assignment7.py
+++ assignment7.py
@@ -1,18 +1,21 @@
 # 1) Create a function with a default parameter "file" storing the file path
 
-def fileHandle(file):
+def fileHandle(file = "assignment7.txt"):
     
-# 2) Open the "file" in append mode
+    # 2) Open the "file" in append mode
 
-    f = open(file,"a")
+    try:
+        f = open(file,"a")
+        
+        # 3) Use writelines() method to add your roll number, name, and class
 
-# 3) Use writelines() method to add your roll number, name, and class
+        f.writelines("Roll number - 115, Name - Vishhal, Class - CO3")
 
-    f.writelines("Roll number - 115, Name - Vishhal, Class - CO3")
-
-# 4) Use readines() method to print your data in the prompt
-    f.close()
-    f = open(file,"r")
-    print(f.readlines())
+        # 4) Use readines() method to print your data in the prompt
+        
+        f.close()
+        f = open(file,"r")
+        print(f.readlines())
+    except IOError: pass
 
 fileHandle("assignment7.txt")
---
+++ assignment8.py
@@ -0,0 +1,16 @@
+# 1) Create 2 classes for single inheritance named respectively A(base class) and B(derived class)
+
+# 2) Create 3 data members in class A: a(private), b(protected) and c(public) initialise their values in 
+# a parameterized constructor
+
+# 3) Create a method known as display in both the classes, to display the values of a,b and c
+
+# 4) While accessing the private member an exception should be raised and a personalized message should 
+# be displayed and the exception should be handled properly so that the rest of the code can get executed
+
+class A:
+    print('class A')
+    
+class B (A):
+    print()
+    
